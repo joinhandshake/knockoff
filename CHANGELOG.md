46e989ea9b4e2546018d031d6f4ec5a1f50b3958
Scott Ringwelski <sgringwe@mtu.edu>
2018-04-24 12:54:39 -0700

    update supported rubies. attempt to fix AR DB config

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## Unreleased
+
+- Drop Ruby 2.2 support
+- Add Ruby 2.5 support
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ ENV['RACK_ENV'] = 'test'
 require 'knockoff'
 
 ActiveRecord::Base.configurations = {
-  'test' => { adapter: 'sqlite3', database: 'tmp/test_db' }
+  test: => { adapter: 'sqlite3', database: 'tmp/test_db' }
 }
 
 # Setup the ENV's for replicas
