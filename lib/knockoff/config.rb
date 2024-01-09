68a59db8f638580840ea6797adb5f4b874792de5
David Kutalek <david.kutalek@HS-R2QGJN31VR.local>
2024-01-08 20:16:12 -0800

    Use config copy when updating because original is frozen

--- lib/knockoff/config.rb
+++ lib/knockoff/config.rb
@@ -35,7 +35,7 @@ module Knockoff
 
     def update_replica_configs(new_configs)
       if ActiveRecord::Base.configurations.configs_for(env_name: 'knockoff_replicas').present?
-        new_configs.symbolize_keys!.merge!(ActiveRecord::Base.configurations.configs_for(env_name: 'knockoff_replicas').first.configuration_hash)
+        new_configs.deep_dup.merge!(ActiveRecord::Base.configurations.configs_for(env_name: 'knockoff_replicas').first.configuration_hash)
       end
 
       @replicas_configurations.each do |key, _config|
